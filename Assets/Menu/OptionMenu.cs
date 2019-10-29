72f28cfb7ba66aca42ac0a5c96e5131742e5cef8
RizkiTrisna <rzkid.ra@gmail.com>
2019-10-30 06:34:28 +0700

    Re Upload

---
+++ Assets/Menu/OptionMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolution;
+
+    private void Start()
+    {
+        resolution = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolution.Length; i++)
+        {
+            string option = resolution[i].width + " x " + resolution[i].height;
+            options.Add(option);
+
+            if (resolution[i].width == Screen.currentResolution.width && resolution[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution res = resolution[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen) {
+        Screen.fullScreen = isFullScreen;
+    }
+
+}
